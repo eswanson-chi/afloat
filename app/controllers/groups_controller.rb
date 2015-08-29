cc1e712048dbec969657906d0dd580b9789715ed
eswanson-chi <ericswanson@Erics-MacBook-Pro.local>
2015-08-28 23:24:34 -0500

    knock on wood

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @groups = Group.all
+    @groups = Group.where( :owner_user_id => current_user.id )
     @user = User.all
 
   end
--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -1,7 +1,7 @@
 class Recommend < ActiveRecord::Base
   validates :my_app_id, :presence => true
-  validates :activity_id, :presence => true, :uniqueness => { :scope => [:my_app_id, :action_id] }
-  validates :action_id, :uniqueness => { :allow_blank => true, :scope => :activity_id }
+  validates :activity_id, :presence => true, :uniqueness => { :scope => [:user_id,:my_app_id, :action_id] }
+  validates :action_id, :uniqueness => { :allow_blank => true, :scope => [:user_id,:my_app_id, :activity_id ]}
 
   belongs_to :my_app
   belongs_to :activity
